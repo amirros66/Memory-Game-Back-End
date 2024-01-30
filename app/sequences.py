447dbb832b7f794bb66299ae0298a396c39b22cb
AldrinKP <aldrinpastores@gmail.com>
2024-01-30 15:44:30 +0100

    Created function for generating sequence

---
+++ app/sequences.py
@@ -0,0 +1,20 @@
+from sqlalchemy.orm import Session
+from . import models, schemas
+import random
+possible_inputs = ["up", "down", "left", "right"]
+
+def generate_sequence(length: int):
+    sequence = []
+    for i in range(length):
+        sequence.append(random.choice(possible_inputs))
+    return sequence
+
+def add_sequence(db: Session):
+    db_sequence = models.Sequence()
+    db.add(db_sequence)
+    db.commit()
+    db.refresh(db_sequence)
+    return db_sequence
+
+sequence = generate_sequence(4)
+print(sequence)
