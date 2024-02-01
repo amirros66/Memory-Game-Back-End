21b565dc05d7b7daa4c63ca328aeac8e694bbdb1
LileoC <LileoC@proton.me>
2024-02-01 16:45:31 +0100

    get rid of game_id from input sequences model

--- app/models.py
+++ app/models.py
@@ -24,7 +24,6 @@ class Game(Base):
     
     user = relationship("User", back_populates="game")
     display_sequence = relationship("DisplaySequence", back_populates="game")
-    input_sequence = relationship("InputSequence", back_populates="game")
     
 class DisplaySequence(Base):
     __tablename__ = "display_sequences"
@@ -46,12 +45,10 @@ class InputSequence(Base):
     
     user_id = Column(Integer, ForeignKey("users.id"))
     display_sequence_id = Column(Integer, ForeignKey("display_sequences.id"))   
-    game_id = Column(Integer, ForeignKey("games.id"))
     
     user = relationship("User", back_populates="input_sequence")
     display_sequence = relationship("DisplaySequence", back_populates="input_sequence")
     score = relationship("Score", back_populates="input_sequence")
-    game= relationship("Game", back_populates="input_sequence")
 
 #For overall scores at Game over
 class Score (Base):
